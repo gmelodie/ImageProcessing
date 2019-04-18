a5489d4fa9f5560fbc9485c85616111c37cb8ee8
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-04-18 17:22:09 -0300

    Begin assignment 2

---
+++ 2-imEnhance/enhance.py
@@ -0,0 +1,22 @@
+# =====================================================
+# Name: Gabriel de Melo Cruz
+# NUSP: 9763043
+# Course code: SCC0251
+# Period: 2019/1
+# Assignment 2: IMAGE ENHANCEMENT
+# =====================================================
+
+
+
+
+def compute_error(reference, generated):
+    return
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
