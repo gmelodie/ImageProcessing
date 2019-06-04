c2107211bfe50108d5ebb2138e4a8c31c9f45775
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-06-04 16:55:56 -0300

    Add working code v2

--- color-segmentation/colorseg.py
+++ color-segmentation/colorseg.py
@@ -7,12 +7,18 @@
 # =====================================================
 
 import numpy as np
-import imageio, random, math
+import imageio, random, sys
 
 
 def normalize(img, new_min, new_max):
     old_min=np.min(img)
     old_max=np.max(img)
+
+    if debug:
+        print('old min: ', old_min)
+        print('new min: ', new_min)
+        print('old max: ', old_max)
+        print('new max: ', new_max)
 
     norm_img = (img - old_min) * ((new_max - new_min)/(old_max - old_min)) \
         + new_min
@@ -48,13 +54,33 @@ def read_params():
 
 #================================= K MEANS ===================================
 
-def update_centroids(centroids, clusters, dset):
+def update_centroids(centroids, classifications, dset):
 
     # TODO: use np.mean(new_array[new_array == 0], axis=0)...
-    pass
+    # TODO: KILL THIS UGLY CODE
+    for centroid_idx in range(len(centroids)):
+        mean_count_aux = 0
+        mean_sum_aux = np.zeros(dset[0].shape)
+
+        # calculate the mean of the elements in each cluster
+        for entry_idx in range(len(dset)):
+            if classifications[entry_idx] == centroid_idx:
+                mean_count_aux += 1
+                mean_sum_aux += dset[entry_idx]
+
+        if mean_count_aux != 0:
+            centroids[centroid_idx] = mean_sum_aux / mean_count_aux
+
+    return centroids
 
 
 def kmeans(params, dset):
+    """
+    dset: numpy array (m*n, j)
+    where m = number of rows in original image
+    n = number of cols in original image
+    j = number of features (1, 2, 3 depending on the case)
+    """
 
     # unpack parameters
     seed = params['seed']
@@ -63,47 +89,54 @@ def kmeans(params, dset):
 
     # define initial centroids
     random.seed(seed)
-    centroids_idx = np.sort(random.sample(range(0, \
-                            dset.shape[0]*dset.shape[1]), k_clusters))
+    centroids_idx = np.sort(random.sample(range(0,dset.shape[0]),k_clusters))
+    centroids = dset[centroids_idx]
 
-    # 1D array, each entry contains the cluster to which that pixel belongs
-    clusters = np.zeros(dset.shape[0])
+    # 1D array, each value contains the cluster to which that entry belongs
+    classifications = np.zeros(len(dset))
 
     for i in range(n_iterations):
-        centroids = dset[centroids_idx]
 
-        for entry in range(dset):
-            distances = np.sqrt(np.sum(centroids - dset[entry])**2, axis=1)
-            clusters[entry] = np.argmin(distances)
+        for entry in range(len(dset)):
+            distances = np.sqrt(np.square((centroids - dset[entry])).sum(axis=1))
+            classifications[entry] = np.argmin(distances)
 
-        centroids = update_centroids(centroids, clusters, dset)
+        if debug:
+            print('classifications')
+            print(classifications)
+            print('centroids')
+            print(centroids)
 
+        centroids = update_centroids(centroids, classifications, dset)
 
-    return clusters
+    return classifications
 
 
 #=========================== PROCESSING FUNCTIONS ============================
 
 
 def rgb(params, original):
-    segmented = kmeans(params, original)
-    pass
+    dset = np.reshape(original, (original.shape[0] * original.shape[1], 3))
+    classifications = kmeans(params, dset)
+    segmented = np.reshape(classifications, \
+                           (original.shape[0], original.shape[1]))
+    return segmented
 
 
 def rgbxy(params, original):
+    # TODO: include xy fields
     pass
 
 
 def luminance(params, original):
+    # TODO: include xy fields
     luminance = np.dot(original, [0.299, 0.587, 0.114])
-    segmented = kmeans(params, luminance)
     pass
 
 
 def luminancexy(params, original):
+    # TODO: include xy fields
     luminancexy = np.dot(original, [0.299, 0.587, 0.114])
-    #TODO: add x and y in luminancexy
-    segmented = kmeans(params, luminancexy)
     pass
 
 
@@ -111,8 +144,14 @@ def luminancexy(params, original):
 
 
 if __name__ == '__main__':
-    params = read_params()
 
+    global debug
+    if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+        debug = True
+    else:
+        debug = False
+
+    params = read_params()
     processing_opts = {
         1: rgb,
         2: rgbxy,
@@ -123,13 +162,10 @@ if __name__ == '__main__':
     # segment image
     option = params['pixel_attr']
     original = imageio.imread(params['original'])
-    clusters = processing_opts[option](params, original)
-    generated = np.reshape(clusters, original.shape)
+    generated = processing_opts[option](params, original)
 
     norm_generated = normalize(generated, 0, 255)
-
     reference = imageio.imread(params['reference'])
-
     error = compute_error(reference, norm_generated)
     print('{0:.4f}'.format(error))
 
